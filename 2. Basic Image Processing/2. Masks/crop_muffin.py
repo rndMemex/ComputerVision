cbec4998e945bf3fa65fbd1fbb7a37d6702609e5
rndmemex <rndmemex@protonmail.com>
2020-12-21 17:42:08 +0000

    extracting muffin from image

--- 2. Basic Image Processing/2. Masks/crop_muffin.py
+++ 2. Basic Image Processing/2. Masks/crop_muffin.py
@@ -3,22 +3,25 @@ import matplotlib.pyplot as plt
 import cv2
 
 
-
-
 def display_canvas(image, cmap = None, fig_size = (10,10)):
     fig, ax = plt.subplots(figsize=fig_size)
     ax.imshow(image, cmap = cmap)
     ax.axis('on')
     plt.show()
 
+def saving_image(image, output):
+    #save your image to output path using opencv
+    path = output
+    cv2.imwrite(os.path.join(path,'crop_out.jpg'), image)
+    cv2.waitKey(0)
+
 
 image = np.flip(cv2.imread('./img/dog_muffin.jpg'), axis=2)
 # display_canvas(image)
-
-
 
 mask = np.zeros(image.shape[:2], dtype='uint8')
 cv2.rectangle(mask, (550,195), (620, 265), 255, -1)
 masked = cv2.bitwise_and(image, image, mask=mask)
 display_canvas(masked)
+saving_image(image, './img')
 
